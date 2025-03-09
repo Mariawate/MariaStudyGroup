6ae41fef29c4c14a23d363e173ab9f0dcadf4e05
Maria <u22749536@tuks.co.za>
2025-03-09 23:26:11 +0200

    Initialize Git repository

--- MariaStudyGroup/Controllers/StudyGroupController.cs
+++ MariaStudyGroup/Controllers/StudyGroupController.cs
@@ -14,11 +14,11 @@ namespace MariaStudyGroup.Controllers
         {
             List<StudyGroupMember> members = new List<StudyGroupMember>
             {
-                new StudyGroupMember { StudentNumber = "u22749536", Name = "Maria", Surname = "Mbambu", EmailAddress = "maria.mbambu@tuks.co.za" },
-                new StudyGroupMember { StudentNumber = "u24595901", Name = "Petrus", Surname = "Chabedi", EmailAddress = "petrus.chabedi@tuks.co.za" },
-                new StudyGroupMember { StudentNumber = "u22557173", Name = "Masilo", Surname = "Makola", EmailAddress = "masilo.makola@tuks.co.za" },
-                new StudyGroupMember { StudentNumber = "u22761421", Name = "Maria Lynn", Surname = "Vewesse", EmailAddress = "maria.vewesse@tuks.co.za" },
-                new StudyGroupMember { StudentNumber = "u23578140", Name = "Kopano", Surname = "Moloi", EmailAddress = "kopano.moloi@tuks.co.za" }
+                new StudyGroupMember { StudentNumber = "u22749536", Name = "Maria", Surname = "Mbambu", EmailAddress = "maria.mbambu@tuks.co.za", MyLink = "~/HTML/Maria.html" },
+                new StudyGroupMember { StudentNumber = "u24595901", Name = "Petrus", Surname = "Chabedi", EmailAddress = "petrus.chabedi@tuks.co.za", MyLink = "~/HTML/Petrus.html"  },
+                new StudyGroupMember { StudentNumber = "u22557173", Name = "Masilo", Surname = "Makola", EmailAddress = "masilo.makola@tuks.co.za" , MyLink = "~/HTML/Masilo.html"},
+                new StudyGroupMember { StudentNumber = "u22761421", Name = "Maria Lynn", Surname = "Vewesse", EmailAddress = "maria.vewesse@tuks.co.za", MyLink = "~/HTML/Lynn.html" },
+                new StudyGroupMember { StudentNumber = "u23578140", Name = "Kopano", Surname = "Moloi", EmailAddress = "kopano.moloi@tuks.co.za", MyLink = "~/HTML/Kopano.html" }
             };
 
             return View(members);
--- MariaStudyGroup/Models/StudyGroupMember.cs
+++ MariaStudyGroup/Models/StudyGroupMember.cs
@@ -10,15 +10,9 @@ namespace MariaStudyGroup.Models
     {
         [Key]
         public string StudentNumber { get; set; }
-
-        [Required]
         public string Name { get; set; }
-
-        [Required]
         public string Surname { get; set; }
-
-        [Required]
-        [EmailAddress]
         public string EmailAddress { get; set; }
+        public string MyLink { get; set; }
     }
 }
